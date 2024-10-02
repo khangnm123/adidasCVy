7ae7045ebd2677d093e4b461f4ac28bdf9898e22
khangnm123 <khangpro2536@gmail.com>
2024-10-02 14:17:00 +0700

    first commit

--- js/price_range.js
+++ js/price_range.js
@@ -19,7 +19,7 @@ window.onload = function () {
     if(gap <= minGap){
       minVal.value = parseInt(maxVal.value) - minGap;
     }
-    minTooltip.innerHTML = "$" + minVal.value + " " +  "-";
+    minTooltip.innerHTML = "$" + minVal.value + " " + "-";
     priceInputMin.value = minVal.value;
     setArea();
   }
@@ -39,5 +39,53 @@ window.onload = function () {
     minTooltip.style.left = (minVal.value / sliderMaxValue) * 100 + "%";
     range.style.right = 100 - (maxVal.value / sliderMaxValue) * 100 + "%";
     maxTooltip.style.right = 100 - (maxVal.value / sliderMaxValue) * 100 + "%";
+  }
+  
+
+
+
+
+
+  window.onload = function () {
+    slideMin1();
+    slideMax1();
+  };
+  
+  const minVal1 = document.querySelector(".min-val1");
+  const maxVal1 = document.querySelector(".max-val1");
+  const priceInputMin1 = document.querySelector(".min-input1");
+  const priceInputMax1 = document.querySelector(".max-input1");
+  const minTooltip1 = document.querySelector(".min-tooltip1");
+  const maxTooltip1 = document.querySelector(".max-tooltip1");
+  const minGap1 = 0;
+  const range1 = document.querySelector(".slider-track1");
+  const sliderMinValue1 = parseInt(minVal1.min);
+  const sliderMaxValue1 = parseInt(maxVal1.max);
+  
+  function slideMin1() {
+    let gap = parseInt(maxVal1.value) - parseInt(minVal1.value);
+    if(gap <= minGap1){
+      minVal1.value = parseInt(maxVal1.value) - minGap1;
+    }
+    minTooltip1.innerHTML = "$" + minVal1.value + " " + "-";
+    priceInputMin1.value = minVal1.value;
+    setArea();
+  }
+  
+  function slideMax1() {
+    let gap = parseInt(maxVal1.value) - parseInt(minVal1.value);
+    if(gap <= minGap1){
+      maxVal1.value = parseInt(minVal1.value) + minGap1;
+    }
+    maxTooltip1.innerHTML = "$" + maxVal1.value;
+    priceInputMax1.value = maxVal1.value;
+    setArea();
+  }
+  
+  function setArea() {
+    range1.style.left = (minVal1.value / sliderMaxValue1) * 100 + "%";
+    minTooltip1.style.left = (minVal1.value / sliderMaxValue1) * 100 + "%";
+    range1.style.right = 100 - (maxVal1.value / sliderMaxValue1) * 100 + "%";
+    maxTooltip1.style.right = 100 - (maxVal1.value / sliderMaxValue1) * 100 + "%";
   }
   
